bc2bedb334311ceb25e862bee41703ddcd254978
Evgeniy Kirianov <85870790+EvgeniyKiryanow@users.noreply.github.com>
2022-07-31 13:08:49 +0300

    add beans

---
+++ src/main/java/evgeniy/spring/course/DjMusic.java
@@ -0,0 +1,9 @@
+package evgeniy.spring.course;
+
+public class DjMusic implements Music{
+
+    @Override
+    public String getSong() {
+        return "Rubick Dj test";
+    }
+}
--- src/main/java/evgeniy/spring/course/MusicPlayer.java
+++ src/main/java/evgeniy/spring/course/MusicPlayer.java
@@ -1,13 +1,45 @@
 package evgeniy.spring.course;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
     private Music music;
 
-//    IoC
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    //    IoC
+    //dependency injection with constructor
     public MusicPlayer(Music music){
         this.music = music;
     }
+
+    public MusicPlayer(){}
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
     public void playMusic(){
+        System.out.println("Playing " + music.getSong());
+    }
+    public void playListMusic(){
         System.out.println("Playing " + music.getSong());
     }
 }
